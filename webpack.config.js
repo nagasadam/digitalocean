60e8e9a131cc1e0518356edba471973ed68be09a
yeswanth varma kanumuri <yeshuRAJ.9593@gmail.com>
2022-01-11 19:03:39 -0500

    file-loader config

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ module.exports = {
     },
     module: {
         rules: [
+            { test: /\.(jpe?g|gif|png|svg|woff|ttf|wav|mp3)$/, loader: "file-loader" },
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
